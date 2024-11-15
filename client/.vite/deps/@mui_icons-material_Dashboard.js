a35964c9c4ea29919a0f5d191f391eee50321eb9
bijay niraula <bijayniraula7@gmail.com>
2024-11-15 21:25:26 +0545

    fix responsiveness of add video page

---
+++ client/.vite/deps/@mui_icons-material_Dashboard.js
@@ -0,0 +1,25 @@
+"use client";
+import "./chunk-C6WWHQR7.js";
+import {
+  createSvgIcon
+} from "./chunk-KPJ46XD7.js";
+import "./chunk-ZCXOU5NB.js";
+import "./chunk-7YS52U7W.js";
+import "./chunk-C2TBYSZ7.js";
+import {
+  require_jsx_runtime
+} from "./chunk-DMZXRQ7W.js";
+import "./chunk-T5IRCJ6S.js";
+import {
+  __toESM
+} from "./chunk-AK4TLUUY.js";
+
+// node_modules/@mui/icons-material/esm/Dashboard.js
+var import_jsx_runtime = __toESM(require_jsx_runtime());
+var Dashboard_default = createSvgIcon((0, import_jsx_runtime.jsx)("path", {
+  d: "M3 13h8V3H3zm0 8h8v-6H3zm10 0h8V11h-8zm0-18v6h8V3z"
+}), "Dashboard");
+export {
+  Dashboard_default as default
+};
+//# sourceMappingURL=@mui_icons-material_Dashboard.js.map
---
+++ client/.vite/deps/@mui_icons-material_Menu.js
@@ -0,0 +1,25 @@
+"use client";
+import "./chunk-C6WWHQR7.js";
+import {
+  createSvgIcon
+} from "./chunk-KPJ46XD7.js";
+import "./chunk-ZCXOU5NB.js";
+import "./chunk-7YS52U7W.js";
+import "./chunk-C2TBYSZ7.js";
+import {
+  require_jsx_runtime
+} from "./chunk-DMZXRQ7W.js";
+import "./chunk-T5IRCJ6S.js";
+import {
+  __toESM
+} from "./chunk-AK4TLUUY.js";
+
+// node_modules/@mui/icons-material/esm/Menu.js
+var import_jsx_runtime = __toESM(require_jsx_runtime());
+var Menu_default = createSvgIcon((0, import_jsx_runtime.jsx)("path", {
+  d: "M3 18h18v-2H3zm0-5h18v-2H3zm0-7v2h18V6z"
+}), "Menu");
+export {
+  Menu_default as default
+};
+//# sourceMappingURL=@mui_icons-material_Menu.js.map
